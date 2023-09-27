34330e4cf608d0f8b9e539184b417b37ea1a1419
MatiasCordich <m.scordich@gmail.com>
2023-09-26 22:52:12 -0300

    entity crated from class

--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,7 +1,23 @@
 // Las entities son las representacion de la informacion que vamos a grabar en la base de datos
 
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 
+@Schema()
 export class Pokemon extends Document{
 
+    // Definimos las caracteristicas de nuestros atributos con el decorador @Prop()
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true
+    })
+    nro: number;
 }
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon)
